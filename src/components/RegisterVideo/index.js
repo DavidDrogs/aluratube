9d75dadcd20987f5c1e89a3a21fa84e346afe0c5
david pereira <davidjimijuba@gmail.com>
2022-11-11 21:27:16 -0300

    aula 5

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyledRegisterVideo } from "./styles";
+import { createClient } from "@supabase/supabase-js";
 
 function useForm(propsDoForm) {
     const [values, setValues] = React.useState(propsDoForm.initialValues);
@@ -21,6 +22,15 @@ function useForm(propsDoForm) {
     };
 }
 
+const PROJECT_URL = "https://gqddwscmnnrfcfebcxiv.supabase.co";
+const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxZGR3c2Ntbm5yZmNmZWJjeGl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgyMDgwNjIsImV4cCI6MTk4Mzc4NDA2Mn0.h1WdEdbqVNP_CEZfn1SazAYJqcGoy9gjlkyZHcRQSDE";
+const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
+
+//obtem miniatura do youtube a partir do url do video
+function  getThumbnail ( url )  {
+    return  `https://img.youtube.com/vi/ ${ url . split ( "v=" ) [ 1 ] } /hqdefault.jpg` ;
+}
+
 export default function RegisterVideo() {
     const formCadastro = useForm({
         initialValues: {titulo: "frost punk", url:"https://youtube.."}
@@ -37,6 +47,21 @@ export default function RegisterVideo() {
                 ?(
                     <form onSubmit={(evento) => {
                         evento.preventDefault();
+
+                        supabase.from("video").insert({
+                            title: formCadastro.values.titulo,
+                            url: formCadastro.values.url,
+                            thumb: getThumbnail(formCadastro.values.url),
+                            playlist: "jogos",
+                         })
+                         .then((oqueveio) => {
+                            console.log(oqueveio);
+                         })
+                         .catch((err) => {
+                            console.log(err);
+                         })
+
+
                         setFormVisivel(false);
                         formCadastro.clearForm();
                     }}>
